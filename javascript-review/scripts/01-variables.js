6e51ee8c9f358dd1e5d0d7cc434cb1c0314d72e7
Laura <luaana788@gmail.com>
2022-04-21 16:52:35 +0200

    feat: reiew classics variables

---
+++ javascript-review/scripts/01-variables.js
@@ -0,0 +1,57 @@
+// classic's
+
+var myName = "Laura Yissel";
+
+
+function showName () {
+    inName = "Asa Butterfield";
+    var surnames = "Moretaⁿ2";
+
+    console.log("inside:", inName);
+    console.log("inside surname:", surnames);
+}
+
+
+showName();
+console.log("out name:", myName);
+
+
+let country = "Spain";
+
+
+
+function  showCountry (){
+    console.log("country:", country);
+}
+
+showCountry();
+
+if(country === "Spain"){
+    let continent = "Europa";
+    console.log(continent)
+}
+
+console.log("country out", country);
+
+for(count = 0; count<= 5; count++){
+    console.log(count)
+
+}
+
+console.log("count:", count);
+
+
+let age = 33;
+
+console.log(age);
+
+age = 34;
+
+console.log(age);
+
+
+const city = "Madrid";
+const birthdate = 1988;
+
+console.log(city, birthdate);
+
